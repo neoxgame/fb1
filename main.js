3adeaca8221fde20b460b940c7b68ffd6b95e146
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-10 10:32:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path')
 const { app, BrowserWindow } = electron
 let win
 
-app.on('ready', function () { win = new BrowserWindow({ width: 800, webPreferences: { nodeIntegration: true, webviewTag: true, preload: path.join(__dirname, 'preload.js')
+app.on('ready', function () { win = new BrowserWindow({ height: 250, width: 350, webPreferences: { nodeIntegration: true, webviewTag: true, preload: path.join(__dirname, 'preload.js')
 }})
 win.loadFile('index.html')
 })
