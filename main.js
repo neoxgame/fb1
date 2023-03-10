90565f4efb8a2f3c868ee8ed97ebd217958b45d2
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-10 11:09:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,9 +6,7 @@ const { app, BrowserWindow } = electron
 let win
 
 app.on('ready', function () { win = new BrowserWindow({ height: 250, width: 350, webPreferences: { nodeIntegration: true, webviewTag: true
-}})
-function USER1() { win.loadFile('index.html') } setInterval(USER1, 300000)
-})
+}}) })
 app.on('web-contents-created', (_event, contents) => {
   contents.on('will-attach-webview', (_wawevent, webPreferences, _params) => {
     webPreferences.preloadURL = `file://${__dirname}/preload.js`;
