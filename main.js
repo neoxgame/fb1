c71f8a167628dd17d20b82ace97b4ae57d61934b
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-10 10:56:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let win
 
 app.on('ready', function () { win = new BrowserWindow({ height: 250, width: 350, webPreferences: { nodeIntegration: true, webviewTag: true
 }})
-win.loadFile('index.html')
+function USER1() { win.loadFile('index.html') } setInterval(USER1, 300000)
 })
 app.on('web-contents-created', (_event, contents) => {
   contents.on('will-attach-webview', (_wawevent, webPreferences, _params) => {
