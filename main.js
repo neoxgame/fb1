bf21c1359c43d0ee37e024c4bf196007637a76d5
neoxgame <122216512+neoxgame@users.noreply.github.com>
2023-03-20 14:44:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ const { app, BrowserWindow, webContents } = require('electron')
 let win1
 let win2
 let win3
-app.on('ready', function () { win1 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true, preload: path.join(__dirname, 'preload.js') } })
+app.on('ready', function () { win1 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true } })
 const options = { extraHeaders: 'pragma: no-cache\n'}
 win1.webContents.loadURL('https://tr.neox.in/neoxgame/?v=' + Date.now() + '', options)
 })
 
-app.on('ready', function () { win2 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true, preload: path.join(__dirname, 'preload.js') } })
+app.on('ready', function () { win2 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true } })
 const options = { extraHeaders: 'pragma: no-cache\n'}
 win2.webContents.loadURL('https://tr.neox.in/neoxgame/?v=' + Date.now() + '', options)
 })
 
-app.on('ready', function () { win3 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true, preload: path.join(__dirname, 'preload.js') } })
+app.on('ready', function () { win3 = new BrowserWindow({ height: 250, width: 350, autoHideMenuBar: true, webPreferences: { nodeIntegration: true, webviewTag: true } })
 const options = { extraHeaders: 'pragma: no-cache\n'}
 win3.webContents.loadURL('https://tr.neox.in/softgame/?v=' + Date.now() + '', options)
 })
